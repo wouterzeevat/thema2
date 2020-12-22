991dc24b86364d5f830e1672434c85de85071562
keimpedijkstra <keimpedijkstra22@gmail.com>
2020-12-22 10:09:04 +0100

    hashed out the insuline move_to line to make a image for comparison

--- ins_render_test.py
+++ ins_render_test.py
@@ -46,7 +46,7 @@ def frame(step):
     INSULIN_RECEPTOR = pdb.PDBMolecule(PATH_LINUX, center=False, offset=[-10, 8, -5])
     INSULIN_RECEPTOR.move_to([0,0,0])
     insulin = INSULIN_RECEPTOR.divide(insulin_pos, 'insulin')
-    insulin.move_to([-100,0,0])
+    #insulin.move_to([-100,0,0])
     
     
     return Scene(camera,
