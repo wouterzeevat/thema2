477d3a5db52367be5b75c14605225d174eee94f8
keimpedijkstra <keimpedijkstra22@gmail.com>
2020-12-24 14:56:28 +0100

    first try at rendering to alphact binding sites

--- ins_render_test.py
+++ ins_render_test.py
@@ -38,11 +38,9 @@ def frame(step):
     if step < 31:
         ins_id, atom_pos = get_ins(PATH_LINUX)
     
-
         camera = Camera('location', [0, 0, -300], 'look_at', [0, 0, 0])
         light = LightSource([0, 0, -100], 'color', [1, 1, 1])
     
-
         insulin_pos = atom_pos["N"] + atom_pos["O"] 
         #these need to be removed because the used insulin (b chain) is from a sheep, for humans the last aa must be removed
         insulin_pos.remove(9997)
@@ -61,8 +59,33 @@ def frame(step):
         y = (30*2) - (2*step)
         insulin.move_offset([x, y, 0])
     
+
+    if step > 30:
+        camera = Camera('location', [0, 0, -200], 'look_at', [0, 0, 0])
+        light = LightSource([0, 0, -100], 'color', [1, 1, 1])
+
+        ins_id, atom_pos = get_ins(PATH_LINUX)
+        alphact_stage_one = atom_pos["P"]
+        alphact_stage_one_sliced = []
+        INSULIN_RECEPTOR_noshow = pdb.PDBMolecule(PATH_LINUX, center=False, offset=[-10, 8, -5])
+        for pos in alphact_stage_one:
+            if pos in range(10014, 10211):
+                alphact_stage_one_sliced.append(pos)
+        alphact_stage_one_sliced_mol = INSULIN_RECEPTOR_noshow.divide(alphact_stage_one_sliced, 'alphact_one')
+        alphact_stage_one_sliced_mol.move_to([-50,0,0])
+        
+        ins_id, atom_pos = get_ins("/homes/kdijkstra/thema2/pdb/4oga.pdb")
+        alphact_stage_two = atom_pos["F"]
+        site_one_complex = pdb.PDBMolecule("/homes/kdijkstra/thema2/pdb/4oga.pdb", center=False, offset=[-10, 8, -5])
+        alphact_stage_two_mol = site_one_complex.divide(alphact_stage_two, "alphact_two")
+        alphact_stage_two_mol.move_to([50,0,0])
+
+        
+
+
+    
     return Scene(camera,
-                 objects=[light] + insulin.povray_molecule + INSULIN_RECEPTOR.povray_molecule)
+                 objects=[light] + insulin.povray_molecule + INSULIN_RECEPTOR.povray_molecule + alphact_stage_one_sliced_mol.povray_molecule + alphact_stage_two_mol.povray_molecule)
 
     
 def main(args):
@@ -80,4 +103,4 @@ def main(args):
 if __name__ == '__main__':
     #sys.exit(main(sys.argv))
     #pypovray.render_scene_to_png(frame)
-    pypovray.render_scene_to_mp4(frame, range(0,30))
+    pypovray.render_scene_to_mp4(frame, 31)
