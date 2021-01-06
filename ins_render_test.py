9c1a5ee4878e5a8463b19dce6e2f884a2efe6c2b
keimpedijkstra <keimpedijkstra22@gmail.com>
2021-01-06 14:34:58 +0100

    changed frame selection

--- ins_render_test.py
+++ ins_render_test.py
@@ -156,7 +156,7 @@ def main(args):
 if __name__ == '__main__':
     #sys.exit(main(sys.argv))
     #pypovray.render_scene_to_png(frame)
-    pypovray.render_scene_to_mp4(frame, range(46,57))
+    pypovray.render_scene_to_mp4(frame, range(36,47))
 
 # + INSULIN_RECEPTOR.povray_molecule
 # + insulin.povray_molecule
